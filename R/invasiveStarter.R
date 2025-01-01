93a50ee29e36ba07b5a96f604d254eb66bf26641
Jeff <doserjef@msu.edu>
2025-01-01 16:49:12 -0500

    fix stuff for tests

--- R/invasiveStarter.R
+++ R/invasiveStarter.R
@@ -268,7 +268,7 @@ invasiveStarter <- function(x, db, grpBy_quo = NULL, polys = NULL,
       dplyr::summarize(cover = sum(COVER_PCT / 100 * SUBPCOND_PROP * aDI * CONDPROP_UNADJ, 
                                    na.rm = TRUE) / 4) %>%
       dplyr::ungroup() %>%
-      dplyr::mutate(AREA_BASIS = PROP_BASIS) %>%
+      dplyr::rename(AREA_BASIS = PROP_BASIS) %>%
       as.data.frame()
 
     # Sum variable(s) up to plot-level and adjust for non-response
